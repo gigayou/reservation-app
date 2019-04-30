63187890a57d4e5ef8caf1166e002bf5faff1117
gigayou <gigayou@163.com>
2019-04-30 13:45:29 +0800

    release 1.0

--- manager/src/main/java/com/ices/reservation/manager/ManagerApplication.java
+++ manager/src/main/java/com/ices/reservation/manager/ManagerApplication.java
@@ -3,13 +3,26 @@ package com.ices.reservation.manager;
 import com.didispace.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableSwagger2Doc
-public class ManagerApplication {
+public class ManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
+	}
+
+//	@Override//为了打包springboot项目
+//	protected SpringApplicationBuilder configure(
+//			SpringApplicationBuilder builder) {
+//		return builder.sources(this.getClass());
+//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ManagerApplication.class);
 	}
 }
